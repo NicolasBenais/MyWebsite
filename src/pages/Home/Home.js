22c2de597689eedb0dc23da856fea00388876204
Nicolas BENAIS <nicolasbenais@MacBook-Air-de-Nicolas.local>
2022-06-22 19:24:06 +0200

    home.js started

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,52 @@
-// import { useEffect, useState } from "react";
-// import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 // Styles
 import styles from "./Home.module.css";
 
 export default function Home() {
-  // const [photos, setPhotos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       // const response = await axios.get();
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
+  const [data, setData] = useState([]);
 
-  return <main className={styles.main}>Home</main>;
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://nbns-my-website.herokuapp.com/pictures`
+        );
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    !isLoading && (
+      <main className={styles.main}>
+        {data.map((item, index) => {
+          return (
+            <div key={index} className={styles.container}>
+              <Link className={styles.link} to="/picture" state={item}>
+                <img
+                  className={styles.image}
+                  src={item.image.secure_url}
+                  alt=""
+                />
+              </Link>
+              <div className={styles.picture_informations}>
+                <span>{item.location}</span>
+                <span>{item.date}</span>
+              </div>
+            </div>
+          );
+        })}
+      </main>
+    )
+  );
 }
